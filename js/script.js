2fb6976bad1a2217aaccd995e61b6695f2db0430
Chris Williams <cwill833@gmail.com>
2019-05-06 13:27:47 -0500

    add updating with eventlisteners

--- js/script.js
+++ js/script.js
@@ -1,32 +1,41 @@
 // event handlers
 const sellButton = document.querySelector('.sell');
 const upgradeButton = document.querySelector('.upgrade');
+const money = document.getElementById('money');
 
 // cached
-let iW = 185;
+let iW = 50;
 let incr = 1;
+let accMoney = 10
+money.textContent = `${accMoney}$`;
 
 // event listeners
 sellButton.addEventListener('click', (evt) => {
     let grow = document.getElementById('grow1');
-    grow.style.width = `${iW}px`;
+    grow.style.width = `${iW}%`;
     console.log(grow.style.width);
-    if(parseInt(grow.style.width.slice(0,3)) <= parseInt(187)){
-        iW += incr;
-    }
-    if(parseInt(grow.style.width.slice(0,3)) >= parseInt(187)){
+    if(parseInt(grow.style.width.slice(0,3)) <= parseInt(100)){iW += incr;}
+    if(parseInt(grow.style.width.slice(0,3)) >= parseInt(100)){
         iW = 0;
+        money.textContent = `${accMoney += 1}$`;
     }
-    console.log(iW)
 })
 
 upgradeButton.addEventListener('click', ()=>{
+    if (incr <= 4){
     incr ++
+    }
     console.log(incr)
 })
 
 
 
+
+
 function myButton(){
-    location.reload();
+    let incr = 1;
+    let accMoney = 10;
+    money.textContent = `${accMoney}$`;
+
+    // location.reload();
 };
