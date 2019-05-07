db4f5203c9821021ee319977947a413a799917d1
Chris Williams <cwill833@gmail.com>
2019-05-07 10:24:29 -0500

    each button works on its own and updates money when complete

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const sellButtonPitcher = document.getElementById('pitcherB');
 const sellButtonIce = document.getElementById('iceB');
 const sellButtonStraw = document.getElementById('strawB');
 const sellButtonUmbrella = document.getElementById('umbrellaB');
-const upgradeButton = document.querySelector('.upgrade');
+const upgradeButtonIce = document.querySelector('.upgrade');
 const money = document.getElementById('money');
 
 // cached
@@ -15,19 +15,17 @@ let speed1 = 500;
 let speed2 = 750;
 let speed3 = 1000;
 let speed4 = 1250;
-let incr = 1;
+let ticks = 0;
 let accMoney = 10;
-money.textContent = `${accMoney}$`;
 
 // event listeners
 sellButtonPitcher.addEventListener('click', load_bar)
 
 sellButtonIce.addEventListener('click', function(){
-    int = setInterval(function(){
+    let int = setInterval(function(){
         if(fill2 < 100){
             fill2 +=25;
             document.getElementById('grow2').style.width = fill2 + '%';
-            console.log(fill2);
         }else{
             money.textContent = `${accMoney += 2}$`;
             fill2 = 0;
@@ -35,11 +33,11 @@ sellButtonIce.addEventListener('click', function(){
             clearInterval(int);
         }
     }, speed2);
-    
+    console.log(int)
 })
 
 sellButtonStraw.addEventListener('click', function(){
-    int = setInterval(function(){
+   let int = setInterval(function(){
         if(fill3 < 100){
             fill3 +=25;
             document.getElementById('grow3').style.width = fill3 + '%';
@@ -55,11 +53,10 @@ sellButtonStraw.addEventListener('click', function(){
 })
 
 sellButtonUmbrella.addEventListener('click', function(){
-    int = setInterval(function(){
+   let int = setInterval(function(){
         if(fill4 < 100){
             fill4 +=25;
             document.getElementById('grow4').style.width = fill4 + '%';
-            console.log(fill4);
         }else{
             money.textContent = `${accMoney += 4}$`;
             fill4 = 0;
@@ -70,23 +67,29 @@ sellButtonUmbrella.addEventListener('click', function(){
     
 })
 
-upgradeButton.addEventListener('click', ()=>{
-    if (incr <= 4 && accMoney >= 2){
+upgradeButtonIce.addEventListener('click', ()=>{
+    let incr = 100;
+    if (ticks <= 2 && accMoney >= 2){
         accMoney -= 2;
         money.textContent = `${accMoney}$`;
-        incr ++;
+        speed1 -= incr;
+        ticks++
     }
-    console.log(incr)
 })
 
 // functions
 
+init();
+
+function init(){
+    money.textContent = `${accMoney}$`;
+}
+
 function load_bar(){
-    int = setInterval(function(){
+   let int = setInterval(function(){
         if(fill1 < 100){
             fill1 +=25;
             document.getElementById('grow1').style.width = fill1 + '%';
-            console.log(fill1);
         }else{
             money.textContent = `${accMoney += 1}$`;
             fill1 = 0;
@@ -94,12 +97,15 @@ function load_bar(){
             clearInterval(int);
         }
     }, speed1);
-    
 }
 
 
 function myButton(){
-    incr = 1;
+    speed1 = 500;
+    speed2 = 750;
+    speed3 = 1000;
+    speed4 = 1250;
+    ticks = 0;
     accMoney = 10;
     money.textContent = `${accMoney}$`;
 };
