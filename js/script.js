17b7903feee21cd8a83351854ae7f818149fa22b
Chris <cwill833@gmail.com>
2019-05-06 23:48:10 -0400

    add functionality to all sell buttons

--- js/script.js
+++ js/script.js
@@ -1,26 +1,74 @@
 // event handlers
-const sellButton = document.querySelector('.sell');
+const sellButtonPitcher = document.getElementById('pitcherB');
+const sellButtonIce = document.getElementById('iceB');
+const sellButtonStraw = document.getElementById('strawB');
+const sellButtonUmbrella = document.getElementById('umbrellaB');
 const upgradeButton = document.querySelector('.upgrade');
 const money = document.getElementById('money');
 
 // cached
-let fill = 0;
+let fill1 = 0;
+let fill2 = 0;
+let fill3 = 0;
+let fill4 = 0;
 let speed1 = 500;
+let speed2 = 750;
+let speed3 = 1000;
+let speed4 = 1250;
 let incr = 1;
-let accMoney = 10
+let accMoney = 10;
 money.textContent = `${accMoney}$`;
 
 // event listeners
-sellButton.addEventListener('click', load_bar)
-//     let grow = document.getElementById('grow1');
-//     grow.style.width = `${iW}%`;
-//     console.log(grow.style.width);
-//     if(parseInt(grow.style.width.slice(0,3)) <= parseInt(100)){iW += incr;}
-//     if(parseInt(grow.style.width.slice(0,3)) >= parseInt(100)){
-//         iW = 0;
-//         money.textContent = `${accMoney += 1}$`;
-//     }
-// })
+sellButtonPitcher.addEventListener('click', load_bar)
+
+sellButtonIce.addEventListener('click', function(){
+    int = setInterval(function(){
+        if(fill2 < 100){
+            fill2 +=25;
+            document.getElementById('grow2').style.width = fill2 + '%';
+            console.log(fill2);
+        }else{
+            money.textContent = `${accMoney += 1}$`;
+            fill2 = 0;
+            document.getElementById('grow2').style.width = fill2 + '%';
+            clearInterval(int);
+        }
+    }, speed2);
+    
+})
+
+sellButtonStraw.addEventListener('click', function(){
+    int = setInterval(function(){
+        if(fill3 < 100){
+            fill3 +=25;
+            document.getElementById('grow3').style.width = fill3 + '%';
+            console.log(fill3);
+        }else{
+            money.textContent = `${accMoney += 1}$`;
+            fill3 = 0;
+            document.getElementById('grow3').style.width = fill3 + '%';
+            clearInterval(int);
+        }
+    }, speed3);
+    
+})
+
+sellButtonUmbrella.addEventListener('click', function(){
+    int = setInterval(function(){
+        if(fill4 < 100){
+            fill4 +=25;
+            document.getElementById('grow4').style.width = fill4 + '%';
+            console.log(fill4);
+        }else{
+            money.textContent = `${accMoney += 1}$`;
+            fill4 = 0;
+            document.getElementById('grow4').style.width = fill4 + '%';
+            clearInterval(int);
+        }
+    }, speed4);
+    
+})
 
 upgradeButton.addEventListener('click', ()=>{
     if (incr <= 4 && accMoney >= 2){
@@ -33,14 +81,14 @@ upgradeButton.addEventListener('click', ()=>{
 
 function load_bar(){
     int = setInterval(function(){
-        if(fill < 100){
-            fill +=10;
-            document.getElementById('grow1').style.width = fill + '%';
-            console.log(fill);
+        if(fill1 < 100){
+            fill1 +=25;
+            document.getElementById('grow1').style.width = fill1 + '%';
+            console.log(fill1);
         }else{
             money.textContent = `${accMoney += 1}$`;
-            fill = 0;
-            document.getElementById('grow1').style.width = fill + '%';
+            fill1 = 0;
+            document.getElementById('grow1').style.width = fill1 + '%';
             clearInterval(int);
         }
     }, speed1);
@@ -56,21 +104,3 @@ function myButton(){
 
     // location.reload();
 };
-
-
-// let fill = 0;
-
-// function load_bar(){
-
-//     window.setInterval(function(){
-
-//         fill += 10;
-
-//         if(fill === 100){
-//             clearInterval();
-//         }
-//         else {
-//             document.getElementById('grow1').style.width = fill + '%';
-//         }
-//     }, 50)
-// }
