15413fdbc95d48947b94146f280f2c505e73c93f
Chris <cwill833@gmail.com>
2019-05-06 23:21:42 -0400

    status bar load with click and updates money

--- js/script.js
+++ js/script.js
@@ -4,32 +4,48 @@ const upgradeButton = document.querySelector('.upgrade');
 const money = document.getElementById('money');
 
 // cached
-let iW = 50;
+let fill = 0;
+let speed1 = 500;
 let incr = 1;
 let accMoney = 10
 money.textContent = `${accMoney}$`;
 
 // event listeners
-sellButton.addEventListener('click', (evt) => {
-    let grow = document.getElementById('grow1');
-    grow.style.width = `${iW}%`;
-    console.log(grow.style.width);
-    if(parseInt(grow.style.width.slice(0,3)) <= parseInt(100)){iW += incr;}
-    if(parseInt(grow.style.width.slice(0,3)) >= parseInt(100)){
-        iW = 0;
-        money.textContent = `${accMoney += 1}$`;
-    }
-})
+sellButton.addEventListener('click', load_bar)
+//     let grow = document.getElementById('grow1');
+//     grow.style.width = `${iW}%`;
+//     console.log(grow.style.width);
+//     if(parseInt(grow.style.width.slice(0,3)) <= parseInt(100)){iW += incr;}
+//     if(parseInt(grow.style.width.slice(0,3)) >= parseInt(100)){
+//         iW = 0;
+//         money.textContent = `${accMoney += 1}$`;
+//     }
+// })
 
 upgradeButton.addEventListener('click', ()=>{
     if (incr <= 4 && accMoney >= 2){
         accMoney -= 2;
-        console.log(accMoney)
         money.textContent = `${accMoney}$`;
         incr ++;
     }
     console.log(incr)
 })
+
+function load_bar(){
+    int = setInterval(function(){
+        if(fill < 100){
+            fill +=10;
+            document.getElementById('grow1').style.width = fill + '%';
+            console.log(fill);
+        }else{
+            money.textContent = `${accMoney += 1}$`;
+            fill = 0;
+            document.getElementById('grow1').style.width = fill + '%';
+            clearInterval(int);
+        }
+    }, speed1);
+    
+}
 
 
 function myButton(){
@@ -40,3 +56,21 @@ function myButton(){
 
     // location.reload();
 };
+
+
+// let fill = 0;
+
+// function load_bar(){
+
+//     window.setInterval(function(){
+
+//         fill += 10;
+
+//         if(fill === 100){
+//             clearInterval();
+//         }
+//         else {
+//             document.getElementById('grow1').style.width = fill + '%';
+//         }
+//     }, 50)
+// }
